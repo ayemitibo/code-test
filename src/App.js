b168b9a4d2add909d09876c76fb2f89f46bfbd51
ayemitibo <ayemitiboabiodun02@gmail.com>
2020-11-08 13:29:11 +0100

    chore : added filtering implementation for date and pin code

---
+++ src/App.js
@@ -0,0 +1,120 @@
+import React, { useEffect, useState } from "react";
+import csvFile from "./Seed_Data.csv";
+
+const App = () => {
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchField, setSearchField] = useState("");
+  const [dateField, setDateField] = useState("");
+  useEffect(() => {
+    var txtFile = new XMLHttpRequest();
+    console.log("here");
+    txtFile.open("GET", csvFile, false);
+    txtFile.onreadystatechange = function () {
+      var lines = txtFile.responseText.split(/\r\n|\n/);
+
+      var result = [];
+
+      var headers = lines[0].split(",");
+
+      for (var i = 1; i < lines.length; i++) {
+        var obj = {};
+        var currentline = lines[i].split(",");
+
+        for (var j = 0; j < headers.length; j++) {
+          obj[headers[j]] = currentline[j]
+            .split(";")
+            .join(",")
+            .replaceAll(":", "-")
+            .replaceAll(",", " ");
+        }
+
+        result.push(obj);
+      }
+      setFilteredData([...result]);
+    };
+    txtFile.send(null);
+  }, []);
+
+  const filterByPinCode = ({ target }) => {
+    setSearchField(target.value.trim());
+  };
+
+  const filterByDate = ({ target }) => {
+    setDateField(target.value.split("-").reverse().join("/"));
+  };
+
+  return (
+    <div className="container">
+      <div className="col">
+        <div className="col-md-12 ">
+          <div className="row justify-content-between">
+            <div className="form-group col-3">
+              <label htmlFor="simpleinput">Pin Code : </label>
+              <input
+                type="text"
+                id="simpleinput"
+                className="form-control"
+                onChange={filterByPinCode}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="example-date">Date : </label>
+              <input
+                className="form-control"
+                onChange={filterByDate}
+                id="example-date"
+                type="date"
+                name="date"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="col-md-12">
+          <table
+            id="example"
+            className="table table-striped table-bordered"
+            style={{ width: "100%" }}
+          >
+            <thead>
+              <tr>
+                <th>Order Id</th>
+                <th>Cust Id</th>
+                <th className="pincode">Pin Code</th>
+                <th>Order Date</th>
+                <th>Item</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData
+                ? filteredData.map((item, index) => {
+                    const pincodesMatch = item.deliveryPincode.includes(
+                      searchField
+                    );
+                    const isSameDate = item.orderDate.startsWith(dateField);
+
+                    const canShowItem = pincodesMatch && isSameDate;
+                    return (
+                      <tr
+                        key={index}
+                        style={{
+                          display: canShowItem ? "" : "none",
+                        }}
+                      >
+                        <td>{item.orderId}</td>
+                        <td>{item.customerId}</td>
+                        <td>{item.deliveryPincode}</td>
+                        <td>{item.orderDate}</td>
+                        <td>{item.items}</td>
+                      </tr>
+                    );
+                  })
+                : null}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/ProductJson.js
@@ -0,0 +1,46 @@
+
+
+const ProductList = [
+    {
+    productUrl : "product-2.jpg",
+    productName : "Piqué Biker Jacket",
+    productPrice : "67.24"
+    },
+    {
+    productUrl : "product-3.jpg",
+    productName : "Multi-pocket Chest Bag",
+    productPrice : "43.48"
+    },   
+    {
+    productUrl : "product-4.jpg",
+    productName : "Diagonal Textured Cap",
+    productPrice : "60.9"
+    },   
+    {
+    productUrl : "product-6.jpg",
+    productName : "Ankle Boots",
+    productPrice : "98.49"
+    },   
+    {
+    productUrl : "product-7.jpg",
+    productName : "T-shirt Contrast Pocket",
+    productPrice : "49.66"
+    },   
+    {
+    productUrl : "product-8.jpg",
+    productName : "Basic Flowing Scarf",
+    productPrice : "26.28"
+    },   
+    {
+    productUrl : "product-9.jpg",
+    productName : "Piqué Biker Jacket",
+    productPrice : "10.28"
+    },   
+    {
+    productUrl : "product-10.jpg",
+    productName : "Multi-pocket Chest Bag",
+    productPrice : "13.28"
+    },   
+]
+
+export default ProductList
