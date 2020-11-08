3b0cdb8fa81feec39bb1428892bf94c23e2eb890
ayemitibo <ayemitiboabiodun02@gmail.com>
2020-11-08 16:39:38 +0100

    Updates

---
+++ precache-manifest.36261e3917f60e0821b9623356d5b265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f47c15f317f6c6a07392b3c3347321",
+    "url": "/code-test/index.html"
+  },
+  {
+    "revision": "6faf8140ac38b23d0b82",
+    "url": "/code-test/static/css/main.be6a321d.chunk.css"
+  },
+  {
+    "revision": "1131e15a36fa780e1eaf",
+    "url": "/code-test/static/js/2.b7d6e305.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code-test/static/js/2.b7d6e305.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6faf8140ac38b23d0b82",
+    "url": "/code-test/static/js/main.e3a58373.chunk.js"
+  },
+  {
+    "revision": "921a3cf2d77c69034c1a",
+    "url": "/code-test/static/js/runtime-main.7ba0cd7e.js"
+  },
+  {
+    "revision": "6c263390eb94b3e51b56add2636b40e3",
+    "url": "/code-test/static/media/Seed_Data.6c263390.csv"
+  }
+]);
